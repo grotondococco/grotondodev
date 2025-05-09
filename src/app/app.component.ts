b8b997569eedf9dc810a1e78e75aa23b890703a8
Giacomo Rotondo Cocco <grotondococco@gmail.com>
2025-05-09 18:46:28 +0200

    color palette draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'grotondodev';
+  title = 'Giacomo Rotondo';
 }
