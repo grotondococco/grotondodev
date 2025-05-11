c3cf4ce47210ebbbb34cb82aacdb0e8ca6b23f32
Giacomo Rotondo Cocco <grotondococco@gmail.com>
2025-05-12 00:03:39 +0200

    ✨ main-page component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -6,7 +6,38 @@ import { Component } from '@angular/core';
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css',
 })
-export class MainPageComponent {
-  message: String = 'Coming soon';
-  message2: String = ':)';
+export class MainPageComponent implements AfterViewInit {
+  activeSection: string = 'about';
+  currentYear: number = new Date().getFullYear();
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  scrollTo(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+      this.activeSection = sectionId;
+    }
+  }
+
+  ngAfterViewInit(): void {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1, // almeno il 50% visibile
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          const section = entry.target.getAttribute('data-section');
+          if (section) {
+            this.activeSection = section;
+          }
+        }
+      }
+    }, options);
+
+    const sections = this.el.nativeElement.querySelectorAll('[data-section]');
+    sections.forEach((section: Element) => observer.observe(section));
+  }
 }
